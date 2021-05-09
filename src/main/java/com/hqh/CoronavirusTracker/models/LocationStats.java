872c3a441962dd25e611523cc4ff0af613889304
qyuh <hoquanghuy0123@gmail.com>
2021-05-09 10:25:05 +0700

    edit model

--- src/main/java/com/hqh/CoronavirusTracker/models/LocationStats.java
+++ src/main/java/com/hqh/CoronavirusTracker/models/LocationStats.java
@@ -4,18 +4,10 @@ import lombok.Data;
 
 @Data
 public class LocationStats {
+    
     private String state;
     private String country;
     private int latestTotalCases;
     private int diffFromPrevDay;
 
-    @Override
-    public String toString() {
-        return "LocationStats{" +
-                "state='" + state + '\'' +
-                ", country='" + country + '\'' +
-                ", latestTotalCases=" + latestTotalCases +
-                ", diffFromPrevDay=" + diffFromPrevDay +
-                '}';
-    }
 }
